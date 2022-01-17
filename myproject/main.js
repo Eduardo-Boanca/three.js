fa5ed7e27ff224cf2ab395bcb69165aaaa66d53e
Eduardo-Boanca <s7813818b@studenti.itisavogadro.it>
2022-01-17 07:59:06 +0100

    three.js

---
+++ myproject/main.js
@@ -0,0 +1,55 @@
+import './style.css'
+
+import * as THREE from 'three';
+
+import { OribitControls } from 'three/examples/jsm/controls/OribitControls';
+
+const scene = new THREE.Scene(); //scene == CONTAINER
+
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000); //aspect ratio
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector('#bg'),
+});
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+camera.position.setZ(30);
+
+renderer.render(scene, camera); //render == DRAW
+
+//Geometry = the {x,y,z} points that makeup a shape
+const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
+
+//Material = the wrapping paper for an object (No light source required for this)
+const material = new THREE.MeshStandardMaterial({ color: 0xFF6347 });
+
+//Mesh = geometry + Material
+const torus = new THREE.Mesh(geometry, material);
+
+scene.add(torus);
+
+const pointLight = new THREE.PointLight(0xffffff);
+pointLight.position.set(40,40,40);
+
+const ambientLight = new THREE.AmbientLight(0xffffff)
+scene.add(pointLight, ambientLight);
+
+const lightHelper = new THREE.PointLightHelper(pointLight)
+const gridHelper = new THREE.GridHelper(200, 50);
+scene.add(lightHelper, gridHelper)
+
+const controls = new OrbitControls(camera, renderer.domElement);
+
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  torus.rotation.x += 0.01;
+  torus.rotation.y += 0.005;
+  torus.rotation.z += 0.01;
+
+  renderer.render(scene, camera);
+}
+
+animate();
